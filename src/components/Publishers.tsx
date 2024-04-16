3619c50804697e9d28c7da876971284cd61afb33
Nami Sunami <nsunami@pm.me>
2024-04-16 21:26:52 +0200

    remove unused components

--- src/components/Publishers.tsx
+++
@@ -1,9 +0,0 @@
-export function Publishers({ id, url, name }) {
-  return (
-    <div>
-      <a href={url} target="_blank" rel="noopener noreferrer">
-        {name[0].name}
-      </a>
-    </div>
-  )
-}
