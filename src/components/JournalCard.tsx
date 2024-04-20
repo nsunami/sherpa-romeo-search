2429c90e8de4a637c5dc145a5690b136303f8e61
Nami Sunami <nsunami@pm.me>
2024-04-20 20:22:56 +0200

    Render high-level info on whether OA is possible The info is only about whether Submitted, Accepted, Published versions can be permitted. The data are reduced from the policies

--- src/components/JournalCard.tsx
+++ src/components/JournalCard.tsx
@@ -1,4 +1,4 @@
-import { Ref, forwardRef, useRef } from "react"
+import { Fragment, Ref, forwardRef, useMemo, useRef } from "react"
 import { SherpaPublicationDataType } from "../api/sherpaTypes"
 
 export function JournalCardInner(
@@ -9,70 +9,124 @@ export function JournalCardInner(
     listed_in_doaj,
     publisher_policy,
     issns,
+    system_metadata,
   }: SherpaPublicationDataType,
   ref: Ref<HTMLDivElement>
 ) {
   const firstTitle = useRef(title[0]?.title)
 
+  const oaPermitted = useMemo(
+    () => [
+      ...new Set(
+        publisher_policy?.flatMap((p) => {
+          return p.permitted_oa?.flatMap((oa) => {
+            return oa.article_version_phrases?.flatMap((p) => p.phrase)
+          })
+        })
+      ),
+    ],
+    [publisher_policy]
+  )
   return (
     <div
       ref={ref}
-      className="relative m-1 min-h-36 rounded-md border bg-slate-50 p-1"
+      className="m-1 flex min-h-36 flex-col rounded-md border bg-slate-50 p-1"
     >
-      <h1 className="font-bold">
-        <a href={url} target="_blank" rel="noopener noreferrer">
-          {firstTitle.current}
-        </a>
-      </h1>
-      <div className="text-gray-500">
-        {publishers.map((publisher, index) => {
-          return (
-            <span key={publisher.publisher.id}>
+      <div data-card-header>
+        <h1 className="font-bold">
+          <a
+            href={system_metadata.uri}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {firstTitle.current}
+          </a>
+        </h1>
+        <div className="text-gray-500">
+          {publishers.map((publisher, index) => {
+            return (
+              <span key={publisher.publisher.id}>
+                <a
+                  href={publisher.publisher.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {publisher.publisher.name[0].name}
+                </a>
+                {index < publishers.length - 1 ? ", " : ""}
+              </span>
+            )
+          })}
+        </div>
+        <div className="my-1 border-t-2"></div>
+      </div>
+      <div data-card-body className="flex-grow">
+        <div data-publisher-policy className="text-gray-500">
+          Open Access possible with:
+          <ul className="list-none text-gray-800">
+            {oaPermitted.map((i, index) => (
+              <li key={index}>
+                {i === "Submitted"
+                  ? "💌 Submitted"
+                  : i === "Accepted"
+                    ? "✅ Accepted"
+                    : i === "Published"
+                      ? "📙 Published"
+                      : ""}{" "}
+                version
+              </li>
+            ))}
+          </ul>
+          <div className="overflow-hidden text-ellipsis text-nowrap text-sm text-gray-500">
+            Policies:{" "}
+            {publisher_policy.map((policy) => {
+              return (
+                <Fragment key={policy.id}>
+                  <div className="overflow-hidden text-ellipsis">
+                    <a
+                      href={policy.uri}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="hover:underline"
+                    >
+                      📝 {policy.internal_moniker}
+                    </a>
+                  </div>
+                </Fragment>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+      <div data-card-footer className="flex items-end justify-between">
+        <div className=" text-xs text-gray-500">
+          <a
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            {issns[0].issn}
+          </a>
+        </div>
+        <div className=" bottom-1 right-1">
+          <button
+            className={`rounded-md border ${listed_in_doaj === "yes" ? "bg-green-100 hover:bg-green-300" : "bg-gray-400 text-gray-200"} px-2 text-sm`}
+            disabled={listed_in_doaj === "no"}
+          >
+            {listed_in_doaj === "yes" ? (
               <a
-                href={publisher.publisher.url}
+                href={`https://doaj.org/toc/${issns[0].issn}`}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                {publisher.publisher.name[0].name}
+                💚 On DOAJ
               </a>
-              {index < publishers.length - 1 ? ", " : ""}
-            </span>
-          )
-        })}
-      </div>
-      <div className="my-1 border-t-2"></div>
-      <div data-publisher-policy className="text-gray-500">
-        {publisher_policy.map((policy) => {
-          return (
-            <div
-              key={policy.id}
-              className="overflow-hidden text-ellipsis text-nowrap text-gray-500"
-              title={policy.internal_moniker}
-            >
-              <a href={policy.uri} target="_blank" rel="noopener noreferrer">
-                {policy.publication_count + 1}: {policy.internal_moniker}
-              </a>
-            </div>
-          )
-        })}
-      </div>
-      <div className="absolute bottom-1 right-1">
-        <button
-          className={`rounded-md border ${listed_in_doaj === "yes" ? "bg-green-100 hover:bg-green-300" : "bg-gray-400 text-gray-200"} px-2 text-sm`}
-          disabled={listed_in_doaj === "no"}
-        >
-          {listed_in_doaj === "yes" ? (
-            <a
-              href={`https://doaj.org/toc/${issns[0].issn}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              💚 On DOAJ
-            </a>
-          ) : (
-            "Not on DOAJ"
-          )}
-        </button>
+            ) : (
+              "Not on DOAJ"
+            )}
+          </button>
+        </div>
       </div>
     </div>
   )
